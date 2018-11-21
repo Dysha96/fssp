d3c0bf8d2fe72dc8832c2cd9b20ae842c4bcc21a
Dysha96 <a.rusanov@robo.finance>
2018-11-21 17:31:12 +0700

    Рабочая реализация Но блочит по 429

--- public/index.php
+++ public/index.php
@@ -5,8 +5,11 @@
  * Date: 14.11.2018
  * Time: 21:19
  */
+$start = microtime(true);
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
@@ -122,7 +125,7 @@ function getResult($token, $task)
         ]
     )->getBody();
     $obj = json_decode($response);
-    var_dump($obj);
+
     return $obj->{'response'}->{'result'}[0]->{'result'};
 }
 
@@ -183,21 +186,63 @@ $type = 1;
 $inCsv = new SplFileObject(dirname(__DIR__) . DIRECTORY_SEPARATOR . getenv('FILE_IN'));
 $inCsv->setFlags(SplFileObject::READ_CSV);
 $outCsv = new SplFileObject(dirname(__DIR__) . DIRECTORY_SEPARATOR . getenv('FILE_OUT'), 'w');
+$outError = new SplFileObject(dirname(__DIR__) . DIRECTORY_SEPARATOR . getenv('FILE_ERROR'), 'w');
 
 
 //Пропуск первой строки
 $inCsv->seek(1);
+$maxLimit = 5;
+$count = 0;
 
-//while ($inCsv->valid()) {
-//    $inArray = $inCsv->fgetcsv();
-//    $customerId = $inArray[0];
-//    $region = $inArray[1];
-//    $firstName = $inArray[2];
-//    $middleName = $inArray[3];
-//    $lastName = $inArray[4];
-//    $inBirthDate = explode('-', $inArray[5]);
-//    $outBirthDate = $inBirthDate[2] . '.' . $inBirthDate[1] . '.' . $inBirthDate[0];
-//
-//    $outArray = [$customerId];
-////    $outCsv->fputcsv($outArray);
-//}
+try {
+    while ($inCsv->valid() && $count < 3) {
+//    $count++;
+
+        $inArray = $inCsv->fgetcsv();
+        $customerId = $inArray[0];
+        $region = $inArray[1];
+        $firstName = $inArray[2];
+        $middleName = $inArray[3];
+        $lastName = $inArray[4];
+        $inBirthDate = explode('-', $inArray[5]);
+        $outBirthDate = $inBirthDate[2] . '.' . $inBirthDate[1] . '.' . $inBirthDate[0];
+
+        $outArray = ['https://www.zaymer.ru/operator/client_info/' . $customerId];
+
+        $task = getSearchPhysicalTask($token, $firstName, $lastName, $birthDate, $region, $secondName);
+
+        sleep(2);
+        $status = getStatus($token, $task);
+
+        $attempt = 0;
+        while (getStatus($token, $task) != 0 && $attempt < $maxLimit) {
+            $attempt++;
+
+            sleep(2);
+            $status = getStatus($token, $task);
+        }
+
+        if ($status != 0) {
+            echo 'https://www.zaymer.ru/operator/client_info/' . $customerId . ' Был пропушен';
+            $outError->fputcsv($inArray);
+            continue;
+        }
+
+        sleep(2);
+        $result = getResult($token, $task);
+
+        if (!empty($result)) {
+            $outArray[] = json_encode($result);
+        } else {
+            $outArray[] = 'Нет записи в ФССП по региону ' . $region;
+        }
+
+        $outCsv->fputcsv($outArray);
+    }
+} catch (RequestException $e) {
+    echo Psr7\str($e->getRequest());
+    if ($e->hasResponse()) {
+        echo Psr7\str($e->getResponse());
+    }
+}
+echo 'Время выполнения скрипта: ' . round(microtime(true) - $start, 4) . ' сек.';
